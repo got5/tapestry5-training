50916dd891a9afdc5b5b156b3a1b543e857a76fe
Emmanuel DEMEY <demey.emmanuel@gmail.com>
2011-07-15 16:32:25 +0200

    Review TP1

--- Correction/TP1/src/main/java/net/atos/mm/formation/tapestry/services/AppModule.java
+++ Correction/TP1/src/main/java/net/atos/mm/formation/tapestry/services/AppModule.java
@@ -1,34 +1,18 @@
 package net.atos.mm.formation.tapestry.services;
 
 import org.apache.tapestry5.SymbolConstants;
-import org.apache.tapestry5.RenderSupport;
-import org.apache.tapestry5.Validator;
 import org.apache.tapestry5.ioc.MappedConfiguration;
-import org.apache.tapestry5.ioc.OrderedConfiguration;
-import org.apache.tapestry5.services.AssetSource;
 
 public class AppModule {
 
 	public static void contributeApplicationDefaults(
 			MappedConfiguration<String, String> configuration) {
 
-		// Set here the code modify the start page name to "Welcome"
-		configuration.add("tapestry.start-page-name", "Welcome");
+		configuration.add(SymbolConstants.START_PAGE_NAME, "Welcome");
 		
 		configuration.add(SymbolConstants.COMPRESS_WHITESPACE, "false");
+
 		configuration.add(SymbolConstants.PRODUCTION_MODE, "false");
 
-		
-	}
-	
-	public static void contributeFieldValidatorSource(
-			MappedConfiguration<String, Validator> configuration, final RenderSupport renderSupport, 
-			final AssetSource assetSource) {
-		
-	}
-	
-	public static void contributeValidationMessagesSource(
-			OrderedConfiguration<String> configuration) {
-		
 	}
 }
