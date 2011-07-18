47e5387dc8719c7fab26b93fe308d9b4d3d1f1c4
Emmanuel DEMEY <demey.emmanuel@gmail.com>
2011-07-18 10:55:33 +0200

    Review PW6

--- Correction/TP6/src/main/java/net/atos/mm/formation/tapestry/pages/Welcome.java
+++ Correction/TP6/src/main/java/net/atos/mm/formation/tapestry/pages/Welcome.java
@@ -6,7 +6,6 @@ import org.apache.tapestry5.EventConstants;
 import org.apache.tapestry5.annotations.InjectPage;
 import org.apache.tapestry5.annotations.OnEvent;
 import org.apache.tapestry5.annotations.Persist;
-import org.apache.tapestry5.annotations.Retain;
 import org.apache.tapestry5.ioc.annotations.Inject;
 import org.slf4j.Logger;
 
@@ -14,17 +13,19 @@ public class Welcome
 {
 
     /**
-     * This variable is used to store in session the first date access on this
+     * This variable is used to store in session the first date access of the user on this
      * page
      */
     @Persist
     private Date sessionStart;
 
     /**
-     * This variable is used to store in session the last message from Hilo Game
+     * This variable is used to store in session the last message from Hilo Game.
+     * 
      */
+    @Persist
     private String messageFromHilo;
-
+    
     /**
      * This object can be returned by event method to redirect to the
      * corresponding page. This page must be injected by Tapestry
@@ -36,7 +37,7 @@ public class Welcome
      * This variable must be initialized only once for this page instance and
      * not be deleted after.
      */
-    @Retain
+    @Persist
     private Long seed;
     
     @Inject
@@ -45,36 +46,39 @@ public class Welcome
     @OnEvent(EventConstants.ACTIVATE)
     public void initializeRandomizer()
     {
-	if (seed == null)
-	{
-	    seed = System.currentTimeMillis();
-	    logger.info("Initialize randomizer with seed :" + seed);
-	}
-	if (sessionStart == null)
-	{
-	    sessionStart = new Date();
-	    logger.info("Initialize first page access time :" + sessionStart);
-	}
-    }
+		if (seed == null)
+		{
+		    seed = System.currentTimeMillis();
+		    
+		    logger.info("Initialize randomizer with seed :" + seed);
+		}
+		if (sessionStart == null)
+		{
+		    sessionStart = new Date();
+		
+		    logger.info("Initialize first page access time :" + sessionStart);
+		}
 
+    }
+    
     public String getMessageFromHilo()
     {
-	return messageFromHilo;
+    	return messageFromHilo;
     }
 
     public void setMessageFromHilo(String messageFromHilo)
     {
-	this.messageFromHilo = messageFromHilo;
+    	this.messageFromHilo = messageFromHilo;
     }
 
     public Date getSessionStart()
     {
-	return sessionStart;
+    	return sessionStart;
     }
 
     public void setSessionStart(Date sessionStart)
     {
-	this.sessionStart = sessionStart;
+    	this.sessionStart = sessionStart;
     }
 
     /**
@@ -87,13 +91,13 @@ public class Welcome
     private Guess startHiloGame()
     {
 
-	logger.info("Initializing Hilo Game");
-
-	// Setup hilo game by using the Guess injected page
-	// There is a corresponding setup method in Guess class
-	guess.setupGame(seed);
-
-	return guess;
+		logger.info("Initializing Hilo Game");
+	
+		// Setup hilo game by using the Guess injected page
+		// There is a corresponding setup method in Guess class
+		guess.setupGame(seed);
+	
+		return guess;
     }
 
     /**
@@ -104,7 +108,8 @@ public class Welcome
      */
     public boolean getLastHiloMessage()
     {
-	return (messageFromHilo != null && !("".equals(messageFromHilo)));
+    	return (messageFromHilo != null && !("".equals(messageFromHilo)));
     }
+    
 
 }
