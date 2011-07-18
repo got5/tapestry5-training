ca5009945634337aa7d539b56c1b0d0e6f960469
Emmanuel DEMEY <demey.emmanuel@gmail.com>
2011-07-18 14:08:20 +0200

    Review PW13

--- Correction/TP13/src/main/java/net/atos/mm/formation/tapestry/pages/Main.java
+++ Correction/TP13/src/main/java/net/atos/mm/formation/tapestry/pages/Main.java
@@ -7,9 +7,9 @@ import net.atos.mm.formation.tapestry.data.User;
 
 import org.apache.tapestry5.Block;
 import org.apache.tapestry5.EventConstants;
-import org.apache.tapestry5.annotations.SessionState;
 import org.apache.tapestry5.annotations.OnEvent;
 import org.apache.tapestry5.annotations.Persist;
+import org.apache.tapestry5.annotations.SessionState;
 import org.apache.tapestry5.ioc.annotations.Inject;
 
 public class Main {
@@ -121,7 +121,7 @@ public class Main {
 	 * 
 	 */
 	public Block getPorfoliosGrid() {
-
+		
 		// Check for the selected mode and return the appropriate block
 		if (GRID.equals(this.mode)) {
 			return this.advancedList;
@@ -130,7 +130,7 @@ public class Main {
 		}
 
 	}
-
+	
 	/**
 	 * This method is used to setup the advanced view mode
 	 * 
