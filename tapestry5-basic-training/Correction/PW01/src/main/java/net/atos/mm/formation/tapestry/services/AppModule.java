c9f108c8b2ea5031b81d5f4bae2b01f3a8fc300b
ffacon <francois.facon@gmail.com>
2014-11-07 14:52:37 +0100

    update from git

--- tapestry5-basic-training/Correction/PW01/src/main/java/net/atos/mm/formation/tapestry/services/AppModule.java
+++ tapestry5-basic-training/Correction/PW01/src/main/java/net/atos/mm/formation/tapestry/services/AppModule.java
@@ -3,17 +3,15 @@ package net.atos.mm.formation.tapestry.services;
 import org.apache.tapestry5.SymbolConstants;
 import org.apache.tapestry5.ioc.MappedConfiguration;
 
+
 public class AppModule {
 
-	public static void contributeApplicationDefaults(
-			MappedConfiguration<String, String> configuration) {
-
-		configuration.add(SymbolConstants.START_PAGE_NAME, "Welcome");
+	
+	public static void contributeApplicationDefaults(MappedConfiguration<String, String> conf){
 		
-		configuration.add(SymbolConstants.COMPRESS_WHITESPACE, "false");
-
-		configuration.add(SymbolConstants.PRODUCTION_MODE, "false");
-
+		conf.add(SymbolConstants.START_PAGE_NAME, "Welcome");
+		conf.add(SymbolConstants.COMPRESS_WHITESPACE, "false");
+		conf.add(SymbolConstants.PRODUCTION_MODE, "false");
+		
 	}
-  	
 }
