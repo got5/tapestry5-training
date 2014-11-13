0713faae13a401e02713ce125574feb0a37859fb
ffacon <francois.facon@gmail.com>
2014-11-13 17:37:44 +0100

    update from git

--- tapestry5-basic-training/Correction/PW05/src/main/java/net/atos/mm/formation/tapestry/pages/Welcome.java
+++ tapestry5-basic-training/Correction/PW05/src/main/java/net/atos/mm/formation/tapestry/pages/Welcome.java
@@ -6,79 +6,60 @@ import org.apache.tapestry5.EventConstants;
 import org.apache.tapestry5.annotations.InjectPage;
 import org.apache.tapestry5.annotations.OnEvent;
 import org.apache.tapestry5.annotations.Persist;
+import org.apache.tapestry5.annotations.Property;
 import org.apache.tapestry5.ioc.annotations.Inject;
 import org.slf4j.Logger;
+
 
 public class Welcome
 {
 
     /**
-     * This variable is used to store in session the first date access of the user on this
+     * This variable is used to store in session the first date access on this
      * page
      */
-    @Persist
+	@Persist
+	@Property
     private Date sessionStart;
 
     /**
-     * This variable is used to store in session the last message from Hilo Game.
-     * 
+     * This variable is used to store in session the last message from Hilo Game
      */
-    @Persist
+	
     private String messageFromHilo;
-    
+
     /**
      * This object can be returned by event method to redirect to the
      * corresponding page. This page must be injected by Tapestry
      */
     @InjectPage
+	@Property
     private Guess guess;
 
     /**
      * This variable must be initialized only once for this page instance and
      * not be deleted after.
      */
-    @Persist
+	@Persist
+	@Property
     private Long seed;
-    
-    @Inject
+
+	@Inject
 	private Logger logger;
-    
-    @OnEvent(EventConstants.ACTIVATE)
-    public void initializeRandomizer()
+	
+	@OnEvent(value=EventConstants.ACTIVATE)
+    public void initializeRandomizer ()
     {
-		if (seed == null)
-		{
-		    seed = System.currentTimeMillis();
-		    
-		    logger.info("Initialize randomizer with seed :" + seed);
-		}
-		if (sessionStart == null)
-		{
-		    sessionStart = new Date();
-		
-		    logger.info("Initialize first page access time :" + sessionStart);
-		}
-
-    }
-    
-    public String getMessageFromHilo()
-    {
-    	return messageFromHilo;
-    }
-
-    public void setMessageFromHilo(String messageFromHilo)
-    {
-    	this.messageFromHilo = messageFromHilo;
-    }
-
-    public Date getSessionStart()
-    {
-    	return sessionStart;
-    }
-
-    public void setSessionStart(Date sessionStart)
-    {
-    	this.sessionStart = sessionStart;
+	if (seed == null)
+	{
+	    seed = System.currentTimeMillis();
+	    logger.info("Initialize randomizer with seed :" + seed);
+	}
+	if (sessionStart == null)
+	{
+	    sessionStart = new Date();
+	    logger.info("Initialize first page access time :" + sessionStart);
+	}
     }
 
     /**
@@ -87,14 +68,11 @@ public class Welcome
      * 
      * @return Guess Tapestry handle this object to redirect the user to thus pa
      */
-    @OnEvent(value = EventConstants.ACTION, component = "hilo")
+	@OnEvent(value = EventConstants.ACTION, component = "hilo")
     private Guess startHiloGame()
     {
-
 		logger.info("Initializing Hilo Game");
-	
-		guess.setupGame(seed);
-	
+		this.guess.setupGame(this.seed);
 		return guess;
     }
 
@@ -110,10 +88,18 @@ public class Welcome
     	{
 	    	//If the User found the good number, we reinit the seed value
 	    	seed = System.currentTimeMillis();
+	    	
 	    	return true;
     	}
+    	
     	return false;
     }
     
-
+    public String getMessageFromHilo(){
+    	return messageFromHilo;
+    }
+    
+    public void setMessageFromHilo(String messageFromHilo){
+    	this.messageFromHilo = messageFromHilo;
+    }
 }
