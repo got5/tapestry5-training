57490315bbc7a7349e3ec9a446f8fc8010775de4
ffacon <francois.facon@gmail.com>
2016-01-23 15:51:31 +0100

    AddPortfolio.java

--- tapestry5-basic-training/Correction/PW17/src/main/java/net/atos/mm/formation/tapestry/pages/portfolio/AddPortfolio.java
+++ tapestry5-basic-training/Correction/PW17/src/main/java/net/atos/mm/formation/tapestry/pages/portfolio/AddPortfolio.java
@@ -2,34 +2,36 @@ package net.atos.mm.formation.tapestry.pages.portfolio;
 
 import net.atos.mm.formation.tapestry.data.Portfolio;
 import net.atos.mm.formation.tapestry.data.User;
+import net.atos.mm.formation.tapestry.data.UserManager;
 import net.atos.mm.formation.tapestry.pages.Login;
-import net.atos.mm.formation.tapestry.services.IUserManager;
+import net.atos.mm.formation.tapestry.pages.Main;
 
 import org.apache.tapestry5.EventConstants;
-import org.apache.tapestry5.annotations.Component;
 import org.apache.tapestry5.annotations.OnEvent;
 import org.apache.tapestry5.annotations.SessionState;
-import org.apache.tapestry5.corelib.components.BeanEditForm;
-import org.apache.tapestry5.ioc.annotations.Inject;
 
 public class AddPortfolio {
 
-/** Used to add the created portfolio to the user */
-	@Inject
-	private IUserManager manager;
+	/** Used to add the created portfolio to the user */
+	private UserManager manager;
 
 	/** Used to get the logged user from session */
 	@SessionState
 	private User loggedUser;
-	
-	@Component(id="addPortfolioForm")
-	private BeanEditForm addPortfolioForm;
-	
+
 	/** Used to verify if a user has logged in */
 	private boolean loggedUserExists;
 
 	/** Used to get the portfolio form */
 	private Portfolio portfolio;
+
+	/**
+	 * Used to create a User Manager instance on page activation.
+	 */
+	@OnEvent(EventConstants.ACTIVATE)
+	public void activateManager() {
+		manager = UserManager.getInstance();	
+	}
 
 	public Portfolio getPortfolio() {
 		return portfolio;
@@ -52,7 +54,8 @@ public class AddPortfolio {
 		// logged in
 		if(loggedUserExists) {
 			manager.addPortfolioToUser(loggedUser, portfolio);
-			return this;
+			//return Main.class;
+			return Main.class;
 		}else{
 			return Login.class;	
 		}
